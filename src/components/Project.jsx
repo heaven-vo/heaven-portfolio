c7883e150abc620e87ddd98e08b75fc7a4b68a3b
heaven-vo <congvo.heaven@gmail.com>
2026-03-13 16:28:33 +0700

    update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -48,7 +48,7 @@ const Project = () => {
     <div className="bg-black text-white py-10" id="projects">
         <div className="container mx-auto px-8 md:px-16 lg:px-24">
             <h2 className="text-4xl font-bold text-center py-10 mb:12">My Projects</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 md:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                 {projects.map((project) => (
                     <div key={project.id} className='bg-gray-800 p-6 rounded-lg hover:shadow-lg
                     transform transition-transform duration-300 hover:scale-105'>
