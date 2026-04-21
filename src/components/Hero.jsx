e8d670a76de7f03a3c1d8620f75b68183aa00c01
heaven-vo <congvo.heaven@gmail.com>
2026-04-21 14:40:29 +0700

    update portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,13 +8,13 @@ const Hero = () => {
         transition-transform duration-300 hover:scale-105'/>
         <h1 className='text-4xl font-bold'>I&apos;m {" "}
             <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Vo Chi Cong</span>
-            , Back-end Engineer Fresher
+            , Software Engineer Fresher
         </h1>
         <p className='mt-4 text-lg text-gray-300'>
         I specialize in architecting high-performance Backend systems and Scalable Microservices.
         </p>
         <div className='mt-8 space-x-4'>
-            <a href="https://www.linkedin.com/in/congdev/">
+            <a href="https://www.linkedin.com/in/heavenvo/">
             <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">Contact With Me</button>
             </a>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -53,7 +53,7 @@ const Project = () => {
                     <div key={project.id} className='bg-gray-800 p-6 rounded-lg hover:shadow-lg
                     transform transition-transform duration-300 hover:scale-105'>
                         <img src={project.image} alt={project.name} className='rounded-lg mb-4
-                        w-full h-48 object-cover' />
+                        w-full h-48 object-cover' /> 
                         <h3 className='text-2xl font-bold mb-2'>{project.name}</h3>
                         <p className='text-gray-400 mb-4'>{project.Tagline}</p>
                         <p className='text-gray-400 mb-4'>{project.technologies}</p>
